f64f3c282bc456a0207f738fc0be6f72f369faf7
grychu999 <grychu999@users.noreply.github.com>
2018-12-09 11:48:13 +0100

    piaty

---
+++ piaty.py
@@ -0,0 +1,25 @@
+
+# jak sie robi liste
+
+samochody=['syrena','polonez', 'opel']
+
+print(samochody[0])
+print(samochody[1])
+
+#wypisuje wszystkie pozycje listy
+for s in samochody:
+    print(s)
+
+for idx in range(len(samochody)):
+    print('idx: '+str(idx)+' : '+samochody[idx])
+
+
+#tabliczka mnozenia do 25
+lista=['jeden','dwa','trzy','cztery','piec']
+lista2=['jeden','dwa','trzy','cztery','piec']
+lista3=['jeden','cztery','dziewiec','szesnascie','dwadziescia piec']
+
+
+
+for i in range(len(lista3)):
+    print(lista3[i])
