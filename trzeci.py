fff568dc3a5493b53ea8a8768ab1ffbec276982d
grychu999 <grychu999@users.noreply.github.com>
2018-12-09 10:58:08 +0100

    trzeci

---
+++ trzeci.py
@@ -0,0 +1,11 @@
+i=100
+j=20
+k=50
+
+
+if i<j and j<k:
+    print('i-te wyrazenie jest najmniejsze')
+elif j>i and j<k:
+    print("j-te wyrazenie jest drugie co do wielkosci")
+else:
+    print('lalal')
